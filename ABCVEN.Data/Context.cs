68b3a43d99346fff18eead8b8611a18dba34d700
Dmitry <kingcity71@ya.ru>
2020-02-23 12:59:07 +0300

    entites update

--- ABCVEN.Data/Context.cs
+++ ABCVEN.Data/Context.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using ABCVEN.Entities;
+using ABCVEN.Entity;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ABCVEN.Data
 {
-    public class Context
+    public class ABCVENContext:DbContext
     {
+        public ABCVENContext()
+        public DbSet<Medicine> Medicines { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
     }
 }
--- ABCVEN.Entity/Producer.cs
+++ ABCVEN.Entity/Producer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ABCVEN.Entities
+namespace ABCVEN.Entity
 {
     public class Producer
     {
