8d55cedb13261395bccf312810197d5aaa1eac97
Dmitry <kingcity71@ya.ru>
2020-02-24 20:08:52 +0300

    models created

---
+++ ABCVEN.Models/SalesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCVEN.Models
+{
+    public class SalesViewModel
+    {
+        public string MedicineName { get; set; }
+        public string SalesCount { get; set; }
+        public string SalesSum { get; set; }
+    }
+}
